1a38595771132fe414eac4ef19a797b9961deb05
wooyeoup-rho <wooyeoup.rho@gmail.com>
2022-10-17 21:32:44 -0300

    2.1 Added missed files

---
+++ app/src/main/java/ca/unb/mobiledev/superduperfitnessapp/StartActivity.kt
@@ -0,0 +1,12 @@
+package ca.unb.mobiledev.superduperfitnessapp
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class StartActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.start_activity)
+    }
+}
